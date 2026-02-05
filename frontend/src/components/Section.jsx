fde9b9635e27bf2b48205ea01fa4d9a12dfd73de
pranjalnikum31 <pranjalnikum31@gmail.com>
2026-02-05 22:39:41 +0530

    add section component and update home page

---
+++ frontend/src/components/Section.jsx
@@ -0,0 +1,15 @@
+export default function Section({ title, items }) {
+  return (
+    <section className="px-10 mt-10">
+      <h2 className="text-xl font-semibold mb-4">{title}</h2>
+
+      <div className="flex gap-6 overflow-x-scroll scrollbar-hide">
+        {items.map(item => (
+          <div key={item.id} className="min-w-[280px]">
+            {item.component}
+          </div>
+        ))}
+      </div>
+    </section>
+  )
+}
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,20 +1,28 @@
-import React from 'react'
-import Navbar from '../components/Navbar'
-import Searchbar from '../components/Searchbar'
-import ListingCard from "../components/ListingCard"
-import { listings } from "../data/listings"
+import React from "react";
+import Navbar from "../components/Navbar";
+import Searchbar from "../components/Searchbar";
+import ListingCard from "../components/ListingCard";
+import { listings } from "../data/listings";
+import Section from "../components/Section";
 function Home() {
   return (
     <>
       <Navbar />
-      <Searchbar/>
-      <div className="px-10 mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
-        {listings.map(item => (
+      <Searchbar />
+      {/* <div className="px-10 mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {listings.map((item) => (
           <ListingCard key={item.id} {...item} />
         ))}
-      </div>
+      </div> */}
+      <Section
+        title="Popular homes"
+        items={listings.map((item) => ({
+          id: item.id,
+          component: <ListingCard {...item} />,
+        }))}
+      />
     </>
-  )
+  );
 }
 
-export default Home
+export default Home;
