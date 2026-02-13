e282e03eb6e1b5c3e6808079f09576ae33fb3883
pranjalnikum31 <pranjalnikum31@gmail.com>
2026-02-13 21:20:10 +0530

    Update auth routes

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bcrypt from "bcryptjs";
 import User from "../models/UserSchema.js";
+import jwt from "jsonwebtoken";
 
 const router = express.Router();
 
@@ -27,5 +28,34 @@ router.post("/signup",async (req,res) => {
         res.status(500).json({ message: "Server error" });
     }
     
+})
+router.post("/login",async (req,res) => {
+    try {
+        const {email,password}=req.body;
+        const user =await User.findOne({email})
+        if(!user){
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        const isMatch= await bcrypt.compare(password,user.password)
+        if(!isMatch){
+            return res.status(400).json({ message: "Invalid credentials" });
+        }
+        
+        const token=jwt.sign(
+            {id:user._id},
+            process.env.JWT_SECRET,
+            {expiresIn:"1d"}
+        );
+        res.json({
+            token,
+            user: {
+                id: user._id,
+                name: user.name,
+                email: user.email
+            }
+        });
+    } catch (error) {
+        res.status(500).json({ message: "Server error" });
+    }
 })
 export default router;
